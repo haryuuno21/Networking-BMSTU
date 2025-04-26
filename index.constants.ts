22bf2ce90e4321e3405feb8efa9237ed49a9463b
haryuuno21 <d.shilenok@mail.ru>
2025-04-26 19:46:03 +0300

    transport imitation

--- index.constants.ts
+++ index.constants.ts
@@ -1,5 +1,5 @@
 export const MARS_PORT: number = 8010;
 export const EARTH_PORT: number = 8000;
 export const HOST_NAME = 'localhost'; // адрес вебсокет сервера
-export const TRANSPORT_PORT = 8002; // порт сервера транспортного уровня
-export const TRANSPORT_HOST_NAME = '192.168.12.172'; // адрес сервера транспортного уровня
+export const TRANSPORT_PORT = 8020; // порт сервера транспортного уровня
+export const TRANSPORT_HOST_NAME = 'localhost'; // адрес сервера транспортного уровня
